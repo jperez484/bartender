e7c78a34627fef57245461edf3002db89dcc78b7
Jesus Infante <jperez484@gmail.com>
2016-06-24 03:16:02 +0000

    En-repo main code file

---
+++ bartender.py
@@ -0,0 +1,23 @@
+import random
+
+questions = {
+    "strong": "Ye like yer drinks strong eh?",
+    "salty": "Ye like it with a salty tangy wangy?",
+    "bitter": "Are ye a lubber who likes it bitter with a no spitter?",
+    "sweet": "Would ye like a bit of sweet poison to tickle yer fancy?",
+    "fruity": "Are ye one for a fruity tooty finish?",
+}
+
+ingredients = {
+    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
+    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+    "sweet": ["sugar cube", "spoonful of honey", "splash of cola"],
+    "fruity": ["slice of melon", "dash of passion fruit", "crushed blueberries"],
+}
+
+def preference():
+
+    print style
+    style=input("please enter your preference")
+
